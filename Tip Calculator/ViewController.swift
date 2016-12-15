64aeb4b8bfb2a2777f5504a5493a58efeb05f026
huan007 <hpn007@ucsd.edu>
2016-12-14 20:47:18 -0800

    First commit. Calculator is working

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipSegment: UISegmentedControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +23,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 
+    @IBAction func calculateTIp(sender: AnyObject) {
+        let tipPercent = [0, 0.18, 0.2, 0.25]
+        
+        let billAmount = Double(billField.text!) ?? 0.00
+        let tipAmount: Double = billAmount * tipPercent[tipSegment.selectedSegmentIndex]
+        let totalAmount: Double = billAmount + tipAmount
+        
+        tipLabel.text = String(format: "$%.2f", tipAmount)
+        totalLabel.text = String(format: "$%.2f", totalAmount)
+    }
 
 }
 
